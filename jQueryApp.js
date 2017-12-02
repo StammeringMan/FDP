f500b1d3ccbeefa64b6b9788e997f66b162eba21
Abhinav Alok <abhinav.alok02@gmail.com>
2017-12-02 22:26:45 +0530

    Add error handling and preloader
    Signed-off-by: Abhinav Alok <abhinav.alok02@gmail.com>

--- jQueryApp.js
+++ jQueryApp.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
-	var myToken = "EAACEdEose0cBAMNlP2TqSZAp7GDjeXaJ5FEKQlEMAf2zxV8irKaLi1hk6rLvEVdylZCrZCSbWrFyqwelZAx3Niw1eUXS4zIZAoIeNXkebc8aXrh8zBA6LnuCBXYr1dWXYJjjI8pFb3krgRp2hcoBCPcUsArPDECSvmNYZCuUxNP3BM4SwbsAY6nTxNgZBLfdV0ZD";
+	var myToken = "EAACEdEose0cBAM5fObOUu0cEAkude3EYMLg1RbEWXmGaPE6V6SZBquZAvfb2cPQuAG2U5J5VPXX14LuLJZBFamYPwN6zlNd5qRcV0jIEuMvrNaNsXyZAEKMZAeSfRKZCU4XEiKk8OnfoR40uqgMkeI4cT0M8g6BTT5J6e1mGRVyayuR21wVHEpcZAVZC4ZBHRLtKpAFxP3ipyOQZDZD";
+	$(".loader").hide();
 
 	function myInfo(){
 		$.ajax("https://graph.facebook.com/me?fields=email, name,birthday&access_token="+myToken,{
@@ -11,11 +12,24 @@ $(document).ready(function(){
 				$("#email").text(response.email);
 				$("#bday").text(response.birthday);
 				
+			},
+			error: function(request, errorType, errorMessage){
+				console.log(request);
+				console.log(errorType);
+				alert(errorMessage);
+			},
+			timeout:2000,
+			beforeSend: function(){
+				$(".loader").show();
+			},
+			complete: function(){
+				$(".loader").hide();
 			}
 		});
 	}	
 	$("#click").on('click', myInfo);
 
+	$(".loader1").hide();
 	function myFeeds(){
 		$.ajax("https://graph.facebook.com/me?fields=feed&access_token="+myToken,{
 			success: function(response){
@@ -26,9 +40,22 @@ $(document).ready(function(){
 				$("#post3").text(response.feed.data[2].story);
 				$("#post4").text(response.feed.data[3].story);
 				$("#post5").text(response.feed.data[4].story);
+			},
+			error: function(request, errorType, errorMessage){
+				console.log(request);
+				console.log(errorType);
+				alert(errorMessage);
+			},
+			timeout: 2000,
+			beforeSend: function () {
+				$(".loader1").show();
+			},
+			complete: function(){
+				$(".loader1").hide();
 			}
 		});
 	}
+
 	$("#feeds").on('click', myFeeds);
 });	
 
